6e1481de2ffc9dc841514b464564d927b97e178c
leandrofalasca <s1098306@studenti.univpm.it>
2021-04-14 15:32:15 +0200

    nice commit

---
+++ home/views/VistaHome.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import (QLineEdit, QApplication, QWidget, QPushButton,
+                             QVBoxLayout, QHBoxLayout, QMessageBox)
+
+
+#nice
+
+class VistaHome(object):
+    pass
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
-if __name__ == '__main__':
-    print('PyCharm')
+from home.views.VistaHome import VistaHome
 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    vista_home = VistaHome()
+    vista_home.show()
+    sys.exit(app.exec_())
