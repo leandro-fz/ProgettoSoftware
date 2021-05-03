59928ece3148d7bb40cd74870a3573e90e196de9
leandrofalasca <74785620+leandrofalasca@users.noreply.github.com>
2021-05-03 12:47:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+
 
 from home.views.VistaHome import MainWindow
 
 if __name__ == '__main__':
-    print("ok")
-    print("okok")
+
     app = QtWidgets.QApplication(sys.argv)
     home = QtWidgets.QMainWindow()
     ui=MainWindow()
