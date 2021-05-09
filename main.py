bee96313e604f3c5e563fdbd6bc01513f1e1ba1c
leandrofalasca <s1098306@studenti.univpm.it>
2021-05-09 12:48:55 +0200

    aggiunte schermate di collegamento del menu e soci

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5 import QtCore, QtGui, QtWidgets
-
+from PyQt5 import QtWidgets
 
 from home.views.VistaHome import Ui_Schermataprincipale
 
