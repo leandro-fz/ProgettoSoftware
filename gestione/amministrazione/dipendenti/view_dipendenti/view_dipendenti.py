c576a0ced10380ad4a85d27d6810dcbc9d92c644
leandrofalasca <s1098306@studenti.univpm.it>
2021-06-11 21:10:06 +0200

    committando commit committuosi

--- gestione/amministrazione/dipendenti/view_dipendenti/view_dipendenti.py
+++ gestione/amministrazione/dipendenti/view_dipendenti/view_dipendenti.py
@@ -1,4 +1,6 @@
-from PyQt5 import QtGui,QtCore,QtWidgets
+from PyQt5 import QtGui, QtCore, QtWidgets
+
+from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
@@ -26,7 +28,7 @@ class Ui_gestionedipendente(QWidget):
         self.font_bottoni = QFont("Yu Gothic UI Light", 12)
 
         self.indietro = QPushButton("⬅️")
-        self.indietro.resize(150,50)
+        self.indietro.resize(150, 50)
         self.indietro.setFont(self.font_bottoni)
         self.indietro.clicked.connect(Ui_gestionedipendente.close)
         self.h_layout.addWidget(self.indietro)
@@ -41,33 +43,37 @@ class Ui_gestionedipendente(QWidget):
         self.h_layout.addWidget(self.inserisci_dipendente)
         self.inserisci_dipendente.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
-
         self.elimina_dipendente = QPushButton("Elimina Dipendente")
         self.elimina_dipendente.setFont(self.font_bottoni)
         self.elimina_dipendente.clicked.connect(self.mostra_elimina_dipendente)
         self.h_layout.addWidget(self.elimina_dipendente)
-        self.indietro.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-
+        self.elimina_dipendente.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
         self.modifica_dipendente = QPushButton("Modifica Dipendente")
         self.modifica_dipendente.setFont(self.font_bottoni)
         self.modifica_dipendente.clicked.connect(self.mostra_modifica_dipendente)
         self.h_layout.addWidget(self.modifica_dipendente)
-        self.indietro.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-
+        self.modifica_dipendente.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
         self.v_layout.addLayout(self.h_layout)
         self.setLayout(self.v_layout)
 
-        # self.setStyleSheet(QWidget(background-image:url(.images/immaginepesisfocata.jpeg))
-        # self.resize(781, 500)
         self.setMinimumSize(781, 500)
         self.setMaximumSize(781, 500)
         self.setWindowTitle("Lista Dipendenti")
         self.setWindowIcon(QtGui.QIcon("images/immaginelogo1.png"))
+
+        # per lo sfondo
+        oImage = QImage("images/immaginepesisfocata.jpeg")
+        sImage = oImage.scaled(QSize(791, 501))
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))
+        self.setPalette(palette)
+
         self.show()
 
     def aggiorna_dati(self):
+
         self.list_view_model = QStandardItemModel(self.list_view)
 
         self.font_item = QFont("Arial", 16)
@@ -80,8 +86,8 @@ class Ui_gestionedipendente(QWidget):
             self.list_view_model.appendRow(item)
         self.list_view.setModel(self.list_view_model)
 
-
     def mostra_inserisci_dipendente(self):
+
         self.inserisci_dipendente = VistaInserisciDipendente(self.controller, self.aggiorna_dati)
         self.inserisci_dipendente.show()
 
@@ -89,14 +95,16 @@ class Ui_gestionedipendente(QWidget):
         self.controller.save_data()
 
     def mostra_elimina_dipendente(self):
+
         try:
-            indice = self.list_view.selectedIndexes()[0].row()
-            da_eliminare = self.controller.get_lista_dipendenti()[indice]
+            index = self.list_view.selectedIndexes()[0].row()
+            da_eliminare = self.controller.get_lista_dipendenti()[index]
+
         except:
-            QMessageBox.critical(self, "Errore", "Seleziona il dipendente da eliminare", QMessageBox.Ok, QMessageBox.Ok)
+            QMessageBox.critical(self, "Errore", "Seleziona un dipendente da eliminare", QMessageBox.Ok, QMessageBox.Ok)
             return
-        risposta = QMessageBox.question(self, "Conferma", "Sei sicuro di volere eliminare il dipendente?",
-                                        QMessageBox.Yes, QMessageBox.No)
+        risposta = QMessageBox.question(self, "Conferma", "Vuoi eliminare il dipendente?",QMessageBox.Yes, QMessageBox.No)
+
         if risposta == QMessageBox.Yes:
             self.controller.elimina_dipendente_by_id(da_eliminare.id)
             QMessageBox.about(self, "Eliminato", "Il dipendente è stato eliminato")
@@ -105,14 +113,14 @@ class Ui_gestionedipendente(QWidget):
             return
 
     def mostra_modifica_dipendente(self):
+
         try:
-            indice = self.list_view.selectedIndexes()[0].row()
-            da_visualizzare = self.controller.get_lista_dipendenti()[indice]
+            index = self.list_view.selectedIndexes()[0].row()
+            da_visualizzare = self.controller.get_lista_dipendenti()[index]
+
         except:
-            QMessageBox.critical(self, "Errore", "Seleziona il dipendente da visualizzare", QMessageBox.Ok,
-                                 QMessageBox.Ok)
+            QMessageBox.critical(self, "Errore", "Seleziona un dipendente da visualizzare", QMessageBox.Ok, QMessageBox.Ok)
             return
-        self.modifica_dipendente = VistaModificaDipendente(ControlloreDipendente(da_visualizzare),
-                                                                 self.aggiorna_dati,
-                                                                 self.controller.get_lista_dipendenti())
+
+        self.modifica_dipendente = VistaModificaDipendente(ControlloreListaDipendenti(da_visualizzare),self.aggiorna_dati,self.controller.get_lista_dipendenti())
         self.modifica_dipendente.show()
