82434d1e40e1cac390e283cdb3e7b7117c0f1829
leandrofalasca <s1098306@studenti.univpm.it>
2021-06-24 12:31:02 +0200

    risolto errore salvataggio file

--- gestione/amministrazione/dipendenti/model_dipendenti/model_dipendenti.py
+++ gestione/amministrazione/dipendenti/model_dipendenti/model_dipendenti.py
@@ -6,15 +6,12 @@ class Insieme_Dipendenti():
     def __init__(self):
         super(Insieme_Dipendenti, self).__init__()
         self.lista_dipendenti = []
-        # print("ok")
-        # k = os.path.isfile("gestione/amministrazione/dipendenti/data_dipendenti/lista_dipendenti_salvata.pickle")
-        # print(k)
         if os.path.isfile("gestione/amministrazione/dipendenti/data_dipendenti/lista_dipendenti_salvata.pickle"):
-            # print("ok2")
             with open("gestione/amministrazione/dipendenti/data_dipendenti/lista_dipendenti_salvata.pickle", "rb") as file:
-                # print("file recuperato")
-                self.lista_dipendenti = pickle.load(file)
-                print("file recuperato2")
+                try:
+                    self.lista_dipendenti = pickle.load(file)
+                except EOFError:
+                    return
 
     def aggiungi_dipendente(self, dipendente):
         self.lista_dipendenti.append(dipendente)
