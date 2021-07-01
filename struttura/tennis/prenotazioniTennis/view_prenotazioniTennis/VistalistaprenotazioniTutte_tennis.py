939cea5133a574a6cc61c6b03a8cac28b9447e85
leandrofalasca <s1098306@studenti.univpm.it>
2021-07-01 15:35:59 +0200

    tennis visualizza tutte prenotazioni

--- struttura/tennis/prenotazioniTennis/view_prenotazioniTennis/VistalistaprenotazioniTutte_tennis.py
+++ struttura/tennis/prenotazioniTennis/view_prenotazioniTennis/VistalistaprenotazioniTutte_tennis.py
@@ -1,6 +1,25 @@
 from PyQt5.QtGui import QFont, QStandardItem, QStandardItemModel, QKeySequence
 from PyQt5.QtWidgets import QListView, QVBoxLayout, QLabel, QWidget, QPushButton, QMessageBox, QShortcut
 from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QDate
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QCalendarWidget, QHBoxLayout, QPushButton
+from PyQt5.QtGui import QFont
+from datetime import datetime
+from PyQt5.QtCore import QDate
+from PyQt5.QtGui import QFont, QStandardItemModel, QStandardItem, QTextCharFormat, QColor
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QCalendarWidget, QComboBox, QCheckBox, QMessageBox, \
+    QPushButton
+from datetime import datetime
+
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QListView, QHBoxLayout, QPushButton, QMessageBox
+from PyQt5.QtGui import QFont, QStandardItemModel, QStandardItem
+from datetime import datetime
+
+from PyQt5 import QtGui, QtCore, QtWidgets
+
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 
 
@@ -19,30 +38,59 @@ class VistaListaPrenotazioniTutte(QWidget):
         self.data = data
 
         self.v_layout = QVBoxLayout()
-        self.font = QFont("Yu Gothic UI Light", 15, 15, True)
+        self.font = QFont("Yu Gothic UI Light", 13, 13, True)
 
         if data is not None:
             self.label_prenotazioni_by_data = QLabel("Arrivi del giorno " + data.strftime("%d/%m/%Y") + ":")
         else:
             self.label_prenotazioni_by_data = QLabel("Tutte le prenotazioni: ")
         self.label_prenotazioni_by_data.setAlignment(Qt.AlignCenter)
+        self.label_prenotazioni_by_data.setFont(QFont("Yu Gothic UI Light", 17))
         self.v_layout.addWidget(self.label_prenotazioni_by_data)
-        self.v_layout.addSpacing(30)
 
         self.lista_prenotazioni = QListView()
         self.aggiorna_dati_prenotazioni()
         self.v_layout.addWidget(self.lista_prenotazioni)
 
-        self.bottone_dettagli_prenotaizone = QPushButton("Dettagli prenotazione")
-        self.bottone_dettagli_prenotaizone.setFont(self.font)
-        self.bottone_dettagli_prenotaizone.clicked.connect(self.dettagli_prenotazione)
+
+        self.h_layout = QHBoxLayout()
+
+
+        self.bottone_indietro = QPushButton("⬅️")
+        self.bottone_indietro.setFont(self.font)
+        self.bottone_indietro.clicked.connect(self.dettagli_prenotazione)
+        self.shortcut_indietro = QShortcut(QKeySequence('Alt+left'), self)
+        self.shortcut_indietro.activated.connect(self.mostra_indietro)
+        self.bottone_indietro.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.h_layout.addWidget(self.bottone_indietro)
+
+
+        self.bottone_dettagli_prenotazione = QPushButton("Dettagli prenotazione")
+        self.bottone_dettagli_prenotazione.setFont(self.font)
+        self.bottone_dettagli_prenotazione.clicked.connect(self.dettagli_prenotazione)
         self.shortcut_open = QShortcut(QKeySequence('Return'), self)
         self.shortcut_open.activated.connect(self.dettagli_prenotazione)
-        self.v_layout.addWidget(self.bottone_dettagli_prenotaizone)
+        self.h_layout.addWidget(self.bottone_dettagli_prenotazione)
+        self.bottone_dettagli_prenotazione.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+
+        self.v_layout.addLayout(self.h_layout)
+
 
         self.setLayout(self.v_layout)
         self.resize(900, 600)
-        self.setWindowTitle("Lista Prenotazioni")
+        self.setWindowTitle("Lista Tutte Prenotazioni")
+
+        self.setMinimumSize(781, 500)
+        self.setMaximumSize(781, 500)
+        self.setWindowTitle("Elenco Prenotazioni Tennis")
+        self.setWindowIcon(QtGui.QIcon("images/immaginelogo1.png"))
+
+        # per lo sfondo
+        oImage = QImage("images/immaginepesisfocata.jpeg")
+        sImage = oImage.scaled(QSize(791, 501))
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))
+        self.setPalette(palette)
 
     def aggiorna_dati_prenotazioni(self):
         self.modello_lista_prenotazioni = QStandardItemModel()
@@ -52,7 +100,6 @@ class VistaListaPrenotazioniTutte(QWidget):
             if self.data == prenotazione.data:
                 item = QStandardItem()
                 item.setText("Prenotazione del " + prenotazione.data.strftime("%d/%m/%Y"))
-                             # + " effettuata da " + prenotazione.email_cliente)
                 item.setEditable(False)
                 item.setFont(self.font)
                 self.modello_lista_prenotazioni.appendRow(item)
@@ -65,16 +112,19 @@ class VistaListaPrenotazioniTutte(QWidget):
 
         self.lista_prenotazioni.setModel(self.modello_lista_prenotazioni)
 
+    def mostra_indietro(self):
+        self.close()
+
     def dettagli_prenotazione(self):
         try:
             indice = self.lista_prenotazioni.selectedIndexes()[0].row()
             lista_prenotazioni_filtrata = []
-            for prenotazione in self.controllore_lista_prenotazioni.get_lista_prenotazioni_tennis():
+            for prenotazione in self.controllore_lista_prenotazioni.get_lista_prenotazioni_tennis1():
                 if prenotazione.data == self.data:
                     lista_prenotazioni_filtrata.append(prenotazione)
             da_visualizzare = lista_prenotazioni_filtrata[indice]
         except:
-            QMessageBox.critical(self, "Errore", "Seleziona la prenotazione da visualizzare", QMessageBox.Ok, QMessageBox.Ok)
+            QMessageBox.critical(self, "Errore", "Seleziona una prenotazione da visualizzare", QMessageBox.Ok, QMessageBox.Ok)
             return
 
         self.vista_prenotazione = VistaPrenotazioneTennis(ControllorePrenotazioneTennis(da_visualizzare))
